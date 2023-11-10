59c9adbcdc2ebdabedc5863b3435459f51d394b7
m.vladmir <m.vladimir@codewizard.team>
2023-11-10 10:39:55 +0300

    Chore: clean app

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,10 +1,11 @@
+import { IconName } from './assets/icons/name';
 import Iconsv2 from './assets/icons/sprite.svg';
 
 type TIcon = {
-  iconName: string;
+  iconName: string | IconName;
 };
 
-const Icon = ({ iconName, ...props }: TIcon) => (
+export const Icon = ({ iconName, ...props }: TIcon) => (
   <svg {...props}>
     <use href={`${Iconsv2}#${iconName}`} />
   </svg>
@@ -16,3 +17,7 @@ export const TimerIcon = ({ ...props }) => (
 export const StarIcon = ({ ...props }) => <Icon iconName='timer' {...props} />;
 export const InfoIcon = ({ ...props }) => <Icon iconName='info' {...props} />;
 export const DdIcon = ({ ...props }) => <Icon iconName='dd' {...props} />;
+export const BlaIcon = ({ ...props }) => <Icon iconName='bla' {...props} />;
+export const BigCupIcon = ({ ...props }) => (
+  <Icon iconName='big-cup' {...props} />
+);
